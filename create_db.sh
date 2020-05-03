97266e4ceb3cfca2f691ff9841fd356c793e306b
Rainer Datzer <rdatz@gmx.de>
2020-05-03 23:33:17 +0200

    updated create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat << ENDOFSQL | sqlite3 /dev/shm/mydb.sqlite
+cat << ENDOFSQL | sqlite3 /etc/apache2/vhosts.sqlite
 drop table if exists vhosts;
 create table vhosts(id integer primary key autoincrement, vhost_name varchar(255), target varchar(255));
 insert into vhosts(vhost_name, target) values('example1.com', '/home/example/htdocs');
